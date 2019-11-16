9c2017b275884d6bd4c0d122de173cf45235a473
hamady <medvallhamady@gmail.com>
2019-11-16 01:29:53 +0000

    add some change

--- js/script.js
+++ js/script.js
@@ -1,56 +1,51 @@
+let inputValue ;
 
-let inputValue = document.getElementById('myInput').value;
+let searchBtn = document.querySelector('#search');
 
-
-// let searchBtn = document.querySelector('#search');
-
-
-
-
-function keyup(e) {
-
-    inputValue = e.target.value;
-    console.log(inputValue)
-}
-
-
+const input = document.querySelector('input');
 
 const myKey = '6c263ed5-a23b-4e4f-a126-79a400ce4dd1';
 
-const url = `https://dictionaryapi.com/api/v3/references/sd4/json/${inputValue}?key=${myKey}`;
+let url;
 
-
-
-
-
-fetch(url)
-.then((resp) => resp.json())
-.then((data) => {
-    
-    document.getElementById('span').innerHTML = `
-        <i class="fa fa-volume-up fa_custom"><audio id="audio" src=""'></audio></i>
-    `
-    document.getElementById('word').innerText = data[0].hwi.hw;
-    document.getElementById('spell').innerText = `/${data[0].hwi.prs[0].mw}/`;
-
-    const ol = document.createElement('ol');
-    document.getElementById('noun').appendChild(ol)
-    ol.innerText = data[0].fl;
-
-    
-    let shortDeff = data[0].shortdef;
-
-    shortDeff.map(function(deff) {
-        ol.appendChild(document.createElement('li')).innerText = deff
-    })
-    console.log(shortDeff)
+input.addEventListener('keyup', (e) => {
+    url = `https://dictionaryapi.com/api/v3/references/sd4/json/${e.target.value}?key=${myKey}`;
 })
-.catch((error) =>{
-    console.error();
 
-});
+searchBtn.addEventListener('click', () => {
+    fetch(url)
+    .then((resp) => resp.json())
+    .then((data) => {
+        console.log(data)
+        document.getElementById('span').innerHTML = `
+            <i class="fa fa-volume-up fa_custom"><audio id="audio" src=""'></audio></i>
+        `
+        document.getElementById('word').innerText = data[0].hwi.hw;
+        document.getElementById('spell').innerText = `/${data[0].hwi.prs[0].mw}/`;
 
 
+        
 
+        document.getElementById('noun').innerHTML = '';
+        let ol = document.createElement('ol');
+        document.getElementById('noun').appendChild(ol).innerText = data[0].fl;
+    
+        
+        let nounShortDeff = data[0].shortdef;
 
+        nounShortDeff.map(function(deff) {
+            
+            ol.appendChild(document.createElement('li')).innerText = deff;
+
+        })
+
+        
+        
+        
+    })
+    .catch((error) =>{
+        console.error();
+
+    });
+})
 
